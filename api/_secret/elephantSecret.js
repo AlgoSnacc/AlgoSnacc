174502f80eac5e088b08dbea75e6a8495b31de5a
parvana07 <peroinitworld@gmail.com>
2020-04-14 16:40:41 -0700

    db model and schema added

---
+++ api/_secret/elephantSecret.js
@@ -0,0 +1,6 @@
+const elephantSecret = {};
+
+elephantSecret.url =
+  "postgres://ekyseghh:OPyJfobk0g7smQy-GWDttS2_7Y5rQ2uK@drona.db.elephantsql.com:5432/ekyseghh";
+
+module.exports = elephantSecret;
--- api/db/model.js
+++ api/db/model.js
@@ -1,1 +1,19 @@
 // establish sql connection
+const { Pool } = require("pg");
+
+const elephantSecret = require("../_secret/elephantSecret");
+
+const pool = new Pool({
+  connectionString: elephantSecret.url,
+});
+
+pool.on("connect", () => {
+  console.log("connected to the db");
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    // console.log('exectued query', text);
+    return pool.query(text, params, callback);
+  },
+};
