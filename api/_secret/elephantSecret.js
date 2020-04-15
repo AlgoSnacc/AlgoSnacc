a5d9b075fa22e8f48ec691f6aaf1eaad3655f960
parvana07 <peroinitworld@gmail.com>
2020-04-15 11:31:54 -0700

    deleted secret folder

--- api/_secret/elephantSecret.js
+++
@@ -1,6 +0,0 @@
-const elephantSecret = {};
-
-elephantSecret.url =
-  "postgres://ekyseghh:OPyJfobk0g7smQy-GWDttS2_7Y5rQ2uK@drona.db.elephantsql.com:5432/ekyseghh";
-
-module.exports = elephantSecret;
