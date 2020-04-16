d514db849a3befe13aeb840e26e550c671c678a9
cychoi00 <yenachoi95@gmail.com>
2020-04-15 17:07:16 -0700

    challenge and explanation screens completed - info hardcoded

--- client/App.jsx
+++ client/App.jsx
@@ -1,19 +1,23 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import LoginScreen from './screens/LoginScreen.jsx';
-import HomeScreen from './screens/HomeScreen.jsx';
+import LoginScreen from "./screens/LoginScreen.jsx";
+import HomeScreen from "./screens/HomeScreen.jsx";
+import ChallengeScreen from "./screens/ChallengeScreen.jsx";
+import ExplanationScreen from "./screens/ExplanationScreen.jsx";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Explanation">
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Challenge" component={ChallengeScreen} />
+        <Stack.Screen name="Explanation" component={ExplanationScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
