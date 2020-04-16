3560eefbddceaa3a33168308f29f1530f095ccb0
cychoi00 <yenachoi95@gmail.com>
2020-04-15 17:23:46 -0700

    adding .DS_Store to delete

--- client/App.jsx
+++ client/App.jsx
@@ -1,23 +1,23 @@
-import "react-native-gesture-handler";
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import LoginScreen from "./screens/LoginScreen.jsx";
-import HomeScreen from "./screens/HomeScreen.jsx";
-import ChallengeScreen from "./screens/ChallengeScreen.jsx";
-import ExplanationScreen from "./screens/ExplanationScreen.jsx";
-import LoginScreen from "./screens/LoginScreen.jsx";
-import SignUp from "./components/SignUp.jsx";
-import BottomTabNav from "./components/BottomTabNav.jsx";
-import SnackScreen from "./screens/SnackScreen.jsx";
+import LoginScreen from './screens/LoginScreen.jsx';
+import HomeScreen from './screens/HomeScreen.jsx';
+import ChallengeScreen from './screens/ChallengeScreen.jsx';
+import ExplanationScreen from './screens/ExplanationScreen.jsx';
+import LoginScreen from './screens/LoginScreen.jsx';
+import SignUp from './components/SignUp.jsx';
+import BottomTabNav from './components/BottomTabNav.jsx';
+import SnackScreen from './screens/SnackScreen.jsx';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Explanation">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Challenge" component={ChallengeScreen} />
