014828e422e210a7b73a0a3184d5c1ee220e4602
cychoi00 <yenachoi95@gmail.com>
2020-04-15 19:40:03 -0700

    bugs fixed - updated to run with ip address

--- api/db/model.js
+++ api/db/model.js
@@ -1,18 +1,18 @@
 // establish sql connection
-const { Pool } = require("pg");
-require("dotenv").config();
+const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.ELEPHANTSECRET,
 });
 
-pool.on("connect", () => {
-  console.log("connected to the db");
+pool.on('connect', () => {
+  console.log('connected to the db');
 });
 
 module.exports = {
   query: (text, params, callback) => {
-    // console.log('exectued query', text);
+    console.log('exectued query', text);
     return pool.query(text, params, callback);
   },
 };
--- client/components/CodeEditor.jsx
+++ client/components/CodeEditor.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { WebView } from 'react-native-webview';
+import { IP } from 'react-native-dotenv';
 
 const CodeEditor = () => {
-  return <WebView source={{ uri: process.ENV.IP + ':4000' }} />;
+  return <WebView source={{ uri: IP + ':4000' }} />;
 };
 
 export default CodeEditor;
