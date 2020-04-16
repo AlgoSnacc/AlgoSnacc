1fd0b24b39bff69441c65702b82b89a55a0663b1
cychoi00 <yenachoi95@gmail.com>
2020-04-15 18:08:59 -0700

    resolving merge conflicts from staging - for real

--- client/components/CodeEditor.jsx
+++ client/components/CodeEditor.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import { WebView } from 'react-native-webview';
 
 const CodeEditor = () => {
-<<<<<<< HEAD
   return <WebView source={{ uri: process.ENV.IP + ':4000' }} />;
-=======
-  return <WebView source={{ uri: 'http://192.168.1.138:4000' }} />;
->>>>>>> 7ea3695c2d336fcf3fba3e6c95b4c09f36591a59
 };
 
 export default CodeEditor;
